845e00c5692f6eeacacf9b187d87d4d771751c04
Lionel <lionelevelyn2008@gmail.com>
2026-06-25 21:43:49 +0700

    bitwise part 1

---
+++ op_bitwise13.py
@@ -0,0 +1,31 @@
+#operator bitwise
+
+a = 9
+b = 5
+
+#bitwise OR |
+c = a | b 
+print("\n=====OR=====")
+print("Nilai :",a, "Binary :",format(a,"08b"))
+print("Nilai :",b, "Binary :",format(b,"08b"))
+print("\n")
+print("Nilai :",c, "Binary :",format(c,"08b"))
+print("\n")
+
+#bitwise AND &
+c = a & b 
+print("\n=====AND=====")
+print("Nilai :",a, "Binary :",format(a,"08b"))
+print("Nilai :",b, "Binary :",format(b,"08b"))
+print("\n")
+print("Nilai :",c, "Binary :",format(c,"08b"))
+print("\n")
+
+#bitwise XOR ^
+c = a ^ b 
+print("\n=====XOR=====")
+print("Nilai :",a, "Binary :",format(a,"08b"))
+print("Nilai :",b, "Binary :",format(b,"08b"))
+print("\n")
+print("Nilai :",c, "Binary :",format(c,"08b"))
+print("\n")
