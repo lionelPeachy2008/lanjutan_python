b9de1bd8a2648d382eb93c9924b3a5d6f65a14a4
Lionel <lionelevelyn2008@gmail.com>
2026-06-26 09:40:37 +0700

    bitwise selesai

--- op_bitwise13.py
+++ op_bitwise13.py
@@ -8,7 +8,7 @@ c = a | b
 print("\n=====OR=====")
 print("Nilai :",a, "Binary :",format(a,"08b"))
 print("Nilai :",b, "Binary :",format(b,"08b"))
-print("\n")
+print("-------------------------------------")
 print("Nilai :",c, "Binary :",format(c,"08b"))
 print("\n")
 
@@ -17,7 +17,7 @@ c = a & b
 print("\n=====AND=====")
 print("Nilai :",a, "Binary :",format(a,"08b"))
 print("Nilai :",b, "Binary :",format(b,"08b"))
-print("\n")
+print("-------------------------------------")
 print("Nilai :",c, "Binary :",format(c,"08b"))
 print("\n")
 
@@ -26,6 +26,31 @@ c = a ^ b
 print("\n=====XOR=====")
 print("Nilai :",a, "Binary :",format(a,"08b"))
 print("Nilai :",b, "Binary :",format(b,"08b"))
+print("-------------------------------------")
+print("Nilai :",c, "Binary :",format(c,"08b"))
 print("\n")
+
+#bitwise NOT ~
+c = ~a
+print("\n=====NOT=====")
+print("Nilai :",a, "Binary :",format(a,"08b"))
+print("Nilai :",c, "Binary :",format(c,"08b"))
+print("-------------------------------------")
+d = 0b0000001001
+e = 0b1111111111
+print("Nilai :",e^d, "Binary :",format(e^d,"08b"))
+print("\n")
+
+#shifting
+#shift right (>>)
+c = a >> 2
+print("\n=====SHIFT RIGHT=====")
+print("Nilai :",a, "Binary :",format(a,"08b"))
+print("Nilai :",c, "Binary :",format(c,"08b"))
+
+#shift left (<<)
+c = a << 2
+print("\n=====SHIFT LEFT=====")
+print("Nilai :",a, "Binary :",format(a,"08b"))
 print("Nilai :",c, "Binary :",format(c,"08b"))
 print("\n")
