80cd690337872c0a58932b3a0fdf92c86e321d69
Lionel <lionelevelyn2008@gmail.com>
2026-06-26 20:56:02 +0700

    eps 15 selesai

---
+++ string15.py
@@ -0,0 +1,22 @@
+#membuat tanda ' jadi string
+print("\nini adalah hari jum\'at")
+
+#backslash
+print("C:\\user\\Lionel")
+
+#backspace
+print("ini \bLionel")
+
+#newlines
+print("baris pertama\nbaris kedua")
+print("lionel pertama\r\nlionel kedua")
+
+#menggunakan raw string
+print(r"c:/newfolder")
+
+#multiline literal string
+print("""
+    nama : lionel
+    tanggal lahir : 20-02-2008
+    tempat lahir : kediri
+      """)
