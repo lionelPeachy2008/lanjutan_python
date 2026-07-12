3a7284957fffab7a55dfc52f53b3d10e9f4fe843
Lionel <lionelevelyn2008@gmail.com>
2026-07-12 20:51:03 +0700

    eps 20 tapi belum paham

---
+++ lat_datetime20.py
@@ -0,0 +1,21 @@
+# latihan date and time 
+
+import datetime as dt
+
+hariini = dt.date.today()
+print(hariini)
+
+print(f"hari ini adalah hari = {hariini:%A}")
+
+print("Masukan tanggal, bulan, dan tahun kelahiran anda: ")
+tanggal = int(input("Tanggal : "))
+bulan = int(input("Bulan : "))
+tahun = int(input("Tahun : "))
+
+tanggal_lahir = dt.date(tahun,bulan,tanggal)
+print(tanggal_lahir)
+print(f"harinya adalah hari : {tanggal_lahir:%A}")
+
+umur_hari = hariini - tanggal_lahir
+umur_tahun = umur_hari.days // 365
+print(f"umur anda sekarang adalah : {umur_tahun} tahun")
